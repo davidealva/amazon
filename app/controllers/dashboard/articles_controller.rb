7f24d772a7a9bc43faeb289b88bf4d8b5e24abce
davidealva <davidealva@gmail.com>
2020-12-08 15:39:14 -0700

    Finish connecting dashboard routes

---
+++ app/controllers/dashboard/articles_controller.rb
@@ -0,0 +1,85 @@
+class Dashboard::ArticlesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :validate_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /articles
+  # GET /articles.json
+  def index
+    @articles = Article.all
+  end
+
+  # GET /articles/1
+  # GET /articles/1.json
+  def show
+  end
+
+  # GET /articles/new
+  def new
+    # @article = Article.new
+    @article = current_user.articles.build
+  end
+
+  # GET /articles/1/edit
+  def edit
+  end
+
+  # POST /articles
+  # POST /articles.json
+  def create
+    # @article = Article.new(article_params)
+    @article = current_user.articles.build(article_params)
+
+    respond_to do |format|
+      if @article.save
+        format.html { redirect_to dashboard_path, notice: 'Article was successfully created.' }
+        format.json { render :show, status: :created, location: @article }
+      else
+        format.html { render :new }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /articles/1
+  # PATCH/PUT /articles/1.json
+  def update
+    respond_to do |format|
+      if @article.update(article_params)
+        format.html { redirect_to dashboard_path, notice: 'Article was successfully updated.' }
+        format.json { render :show, status: :ok, location: @article }
+      else
+        format.html { render :edit }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /articles/1
+  # DELETE /articles/1.json
+  def destroy
+    @article.destroy
+    respond_to do |format|
+      format.html { redirect_to dashboard_path, notice: 'Article was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_article
+      # @article = Article.find(params[:id])
+      @article = current_user.articles.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def article_params
+      params.require(:article).permit(:title, :content, :author)
+    end
+
+    def validate_user
+      if current_user != Article.find(params[:id]).user
+        redirect_to root_path
+      end
+    end
+end
